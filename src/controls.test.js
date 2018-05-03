f987f29e34b963d4379fbf117d5c52ee49614f22
Joe Turner <joe@oampo.co.uk>
2018-05-03 13:38:52 +0100

    Add component tests

--- src/controls.test.js
+++ src/controls.test.js
@@ -4,6 +4,25 @@ import {Controls} from './controls';
 import {increment, decrement} from './actions';
 
 describe('Controls', () => {
-    it('Dispatches the increment action');
-    it('Dispatches the decrement action');
+    it('Dispatches the increment action', () => {
+        const value = 15;
+        const dispatch = jest.fn();
+        const wrapper = mount(<Controls dispatch={dispatch} />);
+        const input = wrapper.find('#amount');
+        input.instance().value = value;
+        const button = wrapper.find('#increment-button');
+        button.simulate('click');
+        expect(dispatch).toHaveBeenCalledWith(increment(value));
+    });
+
+    it('Dispatches the decrement action', () => {
+        const value = 15;
+        const dispatch = jest.fn();
+        const wrapper = mount(<Controls dispatch={dispatch} />);
+        const input = wrapper.find('#amount');
+        input.instance().value = value;
+        const button = wrapper.find('#decrement-button');
+        button.simulate('click');
+        expect(dispatch).toHaveBeenCalledWith(decrement(value));
+    });
 });
