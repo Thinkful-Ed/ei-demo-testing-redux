be25935c0e781e15e8f7c1d4c03fc9fbe644feff
Joe Turner <joe@oampo.co.uk>
2018-05-03 13:18:17 +0100

    Add action tests

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,9 +1,19 @@
 import {increment, INCREMENT, decrement, DECREMENT} from './actions';
 
 describe('increment', () => {
-    it('Should return the action');
+    it('Should return the action', () => {
+        const by = 5;
+        const action = increment(by);
+        expect(action.type).toEqual(INCREMENT);
+        expect(action.by).toEqual(by);
+    });
 });
 
 describe('decrement', () => {
-    it('Should return the action');
+    it('Should return the action', () => {
+        const by = 5;
+        const action = decrement(by);
+        expect(action.type).toEqual(DECREMENT);
+        expect(action.by).toEqual(by);
+    });
 });
